0afb66e96321875f3295cf6b730f46cc98bcc484
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-11 13:33:33 -0600

    Updates

---
+++ precache-manifest.de059ac76d17b458cb079b9684281e0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43859df4e01f663b07a7",
+    "url": "/wakatime-trello/static/js/runtime~main.43859df4.js"
+  },
+  {
+    "revision": "4458e3ac5eb47e4d3595",
+    "url": "/wakatime-trello/static/js/main.4458e3ac.chunk.js"
+  },
+  {
+    "revision": "9046efebc3e3ff6f522b",
+    "url": "/wakatime-trello/static/js/1.9046efeb.chunk.js"
+  },
+  {
+    "revision": "4458e3ac5eb47e4d3595",
+    "url": "/wakatime-trello/static/css/main.b1a7817d.chunk.css"
+  },
+  {
+    "revision": "a8a83a1c94c56788dc9c9ef6ae620fa8",
+    "url": "/wakatime-trello/index.html"
+  }
+];
